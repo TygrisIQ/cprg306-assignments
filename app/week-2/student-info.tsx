d4babc205a7df411df1c56c6c72eec351ea07511
Youssif <dazdelli80@gmail.com>
2026-01-22 13:56:04 -0700

    week 2 assignments

--- app/week-2/student-info.tsx
+++ app/week-2/student-info.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
     <div>
       <p>Youssif Al-Halawche (000979657)</p>
       <Link href="https://github.com/tygrisiq/cprg306-assignments">
-        https://github.com/your-username/cprg306-assignments
+        https://github.com/tygrisiq/cprg306-assignments
       </Link>
     </div>
   );
