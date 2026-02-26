b35572dcf1037c6febbf20a5c611952940c4ac73
Youssif <dazdelli80@gmail.com>
2026-02-26 13:08:20 -0700

    week-6 fix

--- app/week-6/item.tsx
+++ app/week-6/item.tsx
@@ -1,18 +1,18 @@
-import React from "react";
+// import React from "react";
 
-type ItemProps = {
-  name: string;
-  quantity: number;
-  category: string;
-};
+// type ItemProps = {
+//   name: string;
+//   quantity: number;
+//   category: string;
+// };
 
-export default function Item({ name, quantity, category }: ItemProps) {
-  return (
-    <li className="p-2 my-4 bg-slate-900 w-full hover:bg-orange-800 cursor-pointer list-none">
-      <h2 className="text-xl font-bold text-white">{name}</h2>
-      <div className="text-sm text-gray-300">
-        Buy {quantity} in {category}
-      </div>
-    </li>
-  );
-}
+// export default function Item({ name, quantity, category }: ItemProps) {
+//   return (
+//     <li className="p-2 my-4 bg-slate-900 w-full hover:bg-orange-800 cursor-pointer list-none">
+//       <h2 className="text-xl font-bold text-white">{name}</h2>
+//       <div className="text-sm text-gray-300">
+//         Buy {quantity} in {category}
+//       </div>
+//     </li>
+//   );
+// }
